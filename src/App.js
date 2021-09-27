cfab3269d7defb1813324aad0d181d6a50c8ee45
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-27 23:34:25 +0530

    integrated other modules together

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./components/nav.css";
+// import "./components/nav.css";
 import Navmenu from "./components/navmenu";
 import LandingPage from "./pages/Landing";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
