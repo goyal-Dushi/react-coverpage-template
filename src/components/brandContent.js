62833d03aa8a84f8c0265ef517f7dbad3010442f
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-10-03 17:54:16 +0530

    styled content showed in header section

--- src/components/brandContent.js
+++ src/components/brandContent.js
@@ -1,20 +1,23 @@
 function BrandContent() {
   return (
-    <div className='offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12'>
-      <h1> {"GO INDIA"} </h1>
-      <h2 className='tagline'> {"Great Minds Together"} </h2>
-
-      <h2 className='subtitle'>
-        {"The future is fast approaching, & new era of"}
-        <br /> <strong></strong>
+    <div
+      className={
+        "d-flex flex-column justify-content-evenly align-items-center h-100 text-center"
+      }>
+      <h1 className={"display-1 text-uppercase"}> {"my company"} </h1>
+      <h2 className={"display-3 text-capitalize"}>
+        {"bringing people together"}
       </h2>
-      <h4 className='subtitle'>
-        <span
-          className='txt-rotate'
-          data-period='1250'
-          data-rotate='[ "technology services", "products", "digital innovation", "IT Consulting "]'></span>
-      </h4>
-      <h2 className='subtitle'> {"is here"} </h2>
+      <div className={"text-center"}>
+        <h2 className={"display-6"}>{"The New Era Of"}</h2>
+        <h4 className={"display-6"}>
+          <span
+            className={"txt-rotate text-capitalize"}
+            data-period='1250'
+            data-rotate='[ "technology services", "products", "digital innovation", "IT Consulting "]'></span>
+        </h4>
+        <h2 className={"display-6"}> {"Is Here"} </h2>
+      </div>
     </div>
   );
 }
