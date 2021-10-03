4b4a0531230af8d356036f6a6d04a5fd17141e11
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-10-03 17:56:30 +0530

    removed nav css import & unused html elements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-// import "./components/nav.css";
 import Navmenu from "./components/navmenu";
 import LandingPage from "./pages/Landing";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Navmenu />
-        <Switch>
-          <Route exact path='/' component={LandingPage} />
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Navmenu />
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+      </Switch>
+    </Router>
   );
 }
 
