bce471c46769b633b082282741338b11d0ff3b06
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-10-04 09:18:30 +0530

    updated content, related style & image src

--- src/modules/Help.module.js
+++ src/modules/Help.module.js
@@ -17,9 +17,9 @@ function HelpSection() {
             </div>
             <div className={"text-left"}>
               <h4 className={"display-6"}> {"Business Management"} </h4>
-              <p className={"mt-2"}>
+              <p className={"mt-2"} style={{ lineHeight: "1.8" }}>
                 {
-                  "We manage all your business needs with end-to-end development cycle, your investment and trust, we will take care of the business, planning, organizing, leading, and controlling all the requirements with complete support."
+                  "Dolor plaid slow-carb adipisicing eu hoodie godard jianbing skateboard fam vape letterpress. Irony mumblecore etsy, unicorn do echo park kickstarter yr hot chicken polaroid. Laboris occupy mlkshk shabby chic culpa hot chicken tumblr. 3 wolf mo."
                 }
               </p>
               <ListGroup>
@@ -35,7 +35,7 @@ function HelpSection() {
           <Col lg={5} md={12} sm={12} className={"align-self-center"}>
             <img
               data-scroll-reveal={"enter right move 30px over 0.6s after 0.4s"}
-              src={"assets/images/grow.png"}
+              src={"assets/images/grow.webp"}
               className={"rounded img-fluid d-block mx-auto"}
               alt={"grow-img"}
             />
