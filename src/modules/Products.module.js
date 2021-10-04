2c6e56551ad0e14bacfe5012b7b6e3e83dfcf5c8
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-10-04 09:19:15 +0530

    increase margin top

--- src/modules/Products.module.js
+++ src/modules/Products.module.js
@@ -2,7 +2,7 @@ import ProductDisplay from "../components/product";
 
 function ProductsSection() {
   return (
-    <section id={"products"} className={"text-center mt-4 mb-5"}>
+    <section id={"products"} className={"text-center mt-5 mb-5"}>
       <h3 className={"display-3"}> {"Our Products"} </h3>
       <ProductDisplay />
     </section>
